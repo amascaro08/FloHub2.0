d2c068c9c657fff7736967843f8ad5231f147040
Alvii <33187195+amascaro08@users.noreply.github.com>
2025-05-22 05:59:32 +0000

    fixbuild

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { TooltipProvider } from "./components/ui/tooltip";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "./components/ui/toaster";
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
