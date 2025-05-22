7a053fc63e188e14056471e3f23d0aeb7d9a6f46
Alvii <33187195+amascaro08@users.noreply.github.com>
2025-05-22 05:58:05 +0000

    fix

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import App from "./App";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { TooltipProvider } from "./components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
 
 createRoot(document.getElementById("root")!).render(
